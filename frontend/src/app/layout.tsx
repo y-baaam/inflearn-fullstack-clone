bdc83d3cecc91914125b0a9744eeb4628399f08a
young beom <dudqja3674@gmail.com>
2025-10-05 02:45:23 +0900

    frontend project init

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,16 @@
+import "./globals.css";
+import Providers from "../../config/providers";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
