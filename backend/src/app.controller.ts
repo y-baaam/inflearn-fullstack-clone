2af618448a85ca5d2abb41169c7a847dee16017d
young beom <dudqja3674@gmail.com>
2025-10-08 16:11:12 +0900

    [BE] feat: Passport JWT 인증 가드 및 전략 구현
    - AccessTokenStrategy 구현 (passport-jwt)
    - AccessTokenGuard 구현
    - AuthModule 추가
    - Swagger Bearer Auth 설정 추가
    - 테스트 엔드포인트 추가

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { AccessTokenGuard } from './auth/guards/access-token.guard';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import type { Request } from 'express';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
@@ -8,5 +11,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('user-test')
+  @UseGuards(AccessTokenGuard)
+  @ApiBearerAuth('access-token')
+  testUser(@Req() req: Request) {
+    console.log(req.user);
+    return 'test completed';
   }
 }
