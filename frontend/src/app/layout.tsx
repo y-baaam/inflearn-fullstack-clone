f721a32c0b00f75b2b11d6cb64eaeb64029128eb
young beom <dudqja3674@gmail.com>
2025-10-14 17:19:18 +0900

    [FE] feat: 사이트 레이아웃 구현

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,15 +1,22 @@
 import "./globals.css";
 import Providers from "../../config/providers";
+import * as api from "@/lib/api";
+import SiteHeader from "@/components/site-header";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const categories = await api.getAllCategories();
+
   return (
     <html lang="en">
       <body>
-        <Providers>{children}</Providers>
+        <Providers>
+          <SiteHeader categories={categories.data ?? []} />
+          {children}
+        </Providers>
       </body>
     </html>
   );
